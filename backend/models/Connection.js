9e3ae79f4a80d115eb33538bceccce5958b170c3
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-05-06 01:02:52 +0200

    Add Connection.js

---
+++ backend/models/Connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectionSchema = new mongoose.Schema({
+    from: String,
+    to: String,
+    departureTime: String,
+    arrivalTime: String,
+    duration: Number,
+    price: Number,
+    changes: Number
+});
+
+module.exports = mongoose.model("Connection", connectionSchema);
