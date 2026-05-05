909be34417de1eac5ccdfc987f64fe3ccc9fd3b2
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-05-06 00:56:45 +0200

    Connect backend to MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("Mongo error:", err));
 
 app.get("/", (req, res) => {
     res.send("API działa 🚆");
 });
 
-app.listen(3001, () => console.log("Server 3001"));
+app.listen(3001, () => {
+    console.log("Server 3001");
+});
