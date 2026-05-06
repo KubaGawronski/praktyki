6aea99320da4c3fa2350822877b67ad7e0fc95de
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-05-06 09:41:25 +0200

    Add test data endpoint

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,39 @@ app.get("/", (req, res) => {
     res.send("API działa 🚆");
 });
 
+const Connection = require("./models/Connection");
+
+app.get("/connections", async (req, res) => {
+    const data = await Connection.find();
+    res.json(data);
+});
+
+app.post("/connections", async (req, res) => {
+    try {
+        const newConnection = new Connection(req.body);
+        await newConnection.save();
+        res.status(201).json(newConnection);
+    } catch (err) {
+        res.status(500).json({ error: "Błąd zapisu" });
+    }
+});
+
+app.get("/add-test", async (req, res) => {
+    const test = new Connection({
+        from: "Warszawa",
+        to: "Kraków",
+        date: "2025-05-02",
+        departureTime: "08:00",
+        arrivalTime: "11:00",
+        duration: 180,
+        price: 120,
+        changes: 0
+    });
+
+    await test.save();
+    res.send("Dodano!");
+});
+
 app.listen(3001, () => {
     console.log("Server 3001");
 });
