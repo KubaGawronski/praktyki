5f8f40e10d5964ec6785b44014ed9414e40b10f7
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-05-06 19:51:07 +0200

    Display search results in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
+type Connection = {
+  from: string;
+  to: string;
+  departureTime: string;
+  arrivalTime: string;
+  duration: number;
+  price: number;
+  changes: number;
+};
+
 function App() {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [date, setDate] = useState("");
+  const [results, setResults] = useState<Connection[]>([]);
 
   const handleSearch = async () => {
     const res = await fetch(
         `http://localhost:3001/connections?from=${from}&to=${to}&date=${date}`
     );
     const data = await res.json();
-    console.log(data);
+
+    setResults(data); // zamiast console.log
   };
 
   return (
@@ -22,6 +34,17 @@ function App() {
         <input type="date" onChange={e => setDate(e.target.value)} />
 
         <button onClick={handleSearch}>Szukaj</button>
+        <div>
+          {results.map((conn: Connection, index) => (
+              <div key={index} style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
+                <p>{conn.from} → {conn.to}</p>
+                <p>{conn.departureTime} - {conn.arrivalTime}</p>
+                <p>{conn.duration} min</p>
+                <p>{conn.price} zł</p>
+                <p>Przesiadki: {conn.changes}</p>
+              </div>
+          ))}
+        </div>
       </div>
   );
 }
